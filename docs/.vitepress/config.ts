a5aa15d3bb56441e92a7134ffb701b2eea7cb6b9
Locietta <locietta@qq.com>
2022-09-11 18:17:48 +0800

    add icon & meta

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -15,17 +15,34 @@ export default async () => {
       }
     },
     base: '/',
-    head: [['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.16.2/dist/katex.min.css' }]],
+    head: [
+      ['link', { rel: 'icon', href: '/L_32x32.ico' }],
+      ['meta', { name: 'keywords', content: "Locietta's Blog" }],
+      ['meta', { name: 'description', content: "Lost Pieces - Locietta's Blog" }],
+      ['meta', { name: 'author', content: 'Locietta' }],
+      ['meta', { name: 'robots', content: 'all' }],
+      ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.0.0/css/regular.min.css' }],
+      ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.0.0/css/all.min.css' }],
+      ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.16.2/dist/katex.min.css' }]
+    ],
     themeConfig: {
       posts: await getPosts(),
       pageSize: pageSize,
+      website: 'https://github.com/Locietta/blog-lost-pieces',
+      logo: '/favicon.ico',
 
       nav: [
         { text: 'Home', link: '/' },
         { text: 'Archives', link: '/pages/archives' },
         { text: 'Tags', link: '/pages/tags' },
         { text: 'About', link: '/pages/about' },
-        { text: 'Github', link: 'https://github.com/Locietta' } // -- External link test
+        {
+          text: 'External',
+          items: [
+            { text: 'Github', link: 'https://github.com/Locietta' },
+            { text: 'Zhihu', link: 'https://www.zhihu.com/people/wang-ling-xin-94' }
+          ]
+        }
       ]
     },
     srcExclude: ['README.md']
