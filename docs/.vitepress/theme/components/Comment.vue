b4544c5a003adc116e155da9ff5964606bc558f4
Locietta <locietta@qq.com>
2023-01-05 18:29:26 +0800

    feat(comment): use official giscus component

--- docs/.vitepress/theme/components/Comment.vue
+++ docs/.vitepress/theme/components/Comment.vue
@@ -1,36 +1,15 @@
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue'
-import { useData, useRoute } from 'vitepress'
 
-const { frontmatter } = useData()
-const giscusRef = ref()
-const { path } = useRoute()
+import Giscus from '@giscus/vue'
 
-console.log(frontmatter.value.page)
-console.log(path)
-
-onMounted(() => {
-    let giscus = document.createElement('script')
-    giscus.async = true
-    giscus.setAttribute('src', 'https://giscus.app/client.js')
-    giscus.setAttribute('data-repo', 'Locietta/blog-lost-pieces')
-    giscus.setAttribute('data-repo-id', 'R_kgDOH-URKw')
-    giscus.setAttribute('data-category', 'Announcements')
-    giscus.setAttribute('data-category-id', 'DIC_kwDOH-URK84CTZKy')
-    giscus.setAttribute('data-mapping', 'pathname')
-    giscus.setAttribute('data-strict', '0')
-    giscus.setAttribute('data-reactions-enabled', '1')
-    giscus.setAttribute('data-emit-metadata', '0')
-    giscus.setAttribute('data-input-position', 'top')
-    giscus.setAttribute('data-theme', 'preferred_color_scheme')
-    giscus.setAttribute('data-loading', 'lazy')
-    giscus.setAttribute('crossorigin', 'anonymous')
-    giscusRef.value.appendChild(giscus)
-})
 </script>
 
 <template>
-    <div ref="giscusRef"></div>
+    <div class="comments-container">
+        <Giscus id="comments" repo="Locietta/blog-lost-pieces" repoId="R_kgDOH-URKw" category="Announcements"
+            categoryId="DIC_kwDOH-URK84CTZKy" mapping="pathname" reactionsEnabled="1" emitMetadata="0"
+            inputPosition="top" theme="light" strict="0" loading="lazy" />
+    </div>
 </template>
 
 <style>
