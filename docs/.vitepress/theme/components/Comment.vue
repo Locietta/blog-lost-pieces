c87a48c47d6c60300912a5cc84e5e6a7269822eb
Locietta <locietta@qq.com>
2023-01-06 17:58:51 +0800

    dep: remove `@giscus/vue`, use handwritten alternative
    this resolves the issue that comment component is not updated when
    toggle pages via nav bar

--- docs/.vitepress/theme/components/Comment.vue
+++ docs/.vitepress/theme/components/Comment.vue
@@ -1,21 +1,41 @@
-<script lang="ts" setup>
-
-import Giscus from '@giscus/vue'
-
-</script>
-
 <template>
-    <div class="comments-container">
-        <Giscus id="comments" repo="Locietta/blog-lost-pieces" repoId="R_kgDOH-URKw" category="Announcements"
-            categoryId="DIC_kwDOH-URK84CTZKy" mapping="pathname" reactionsEnabled="1" emitMetadata="0"
-            inputPosition="top" theme="light" strict="0" loading="lazy" />
+    <div class="comments">
+        <component v-if="showComment" :is="'script'" src="https://giscus.app/client.js" 
+            :data-repo="'Locietta/blog-lost-pieces'" 
+            :data-repo-id="'R_kgDOH-URKw'"
+            :data-category="'Announcements'"
+            :data-category-id="'DIC_kwDOH-URK84CTZKy'" 
+            :data-mapping="'pathname'"
+            data-reactions-enabled="1" data-emit-metadata="0"
+            :data-input-position="'top'" :data-theme="isDark ? 'dark' : 'light'"
+            :data-lang="'en'" crossorigin="anonymous"
+            :data-loading="'lazy'" async>
+        </component>
     </div>
 </template>
 
-<style>
-/*global  style*/
-.giscus {
+<script lang="ts" setup>
+import { useData, useRoute } from 'vitepress'
+import { ref, watch } from 'vue'
+
+const { isDark } = useData()
+
+const route = useRoute()
+const showComment = ref(true)
+watch(
+    () => route.path,
+    () => {
+        showComment.value = false
+        setTimeout(() => {
+            showComment.value = true
+        }, 150)
+    }
+)
+</script>
+<style scoped>
+.comments {
     padding-top: 1.0625rem;
-    max-width: inherit !important;
+    width: 100%;
+    text-align: center;
 }
 </style>
