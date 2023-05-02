643d876f64f6eacc47b265965fd644b415a2da27
Locietta <locietta@qq.com>
2023-05-02 23:45:03 +0800

    fix: surpass hydration mismatch in embeded tweet

--- .vitepress/theme/components/Tweet.vue
+++ .vitepress/theme/components/Tweet.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
-    <RemoteJs src="https://platform.twitter.com/widgets.js"></RemoteJs>
-    <slot></slot>
-  </div>
+  <ClientOnly>
+    <div>
+      <RemoteJs src="https://platform.twitter.com/widgets.js"></RemoteJs>
+      <slot></slot>
+    </div>
+  </ClientOnly>
 </template>
 <script setup lang="ts">
 import RemoteJs from './RemoteJs.vue'
