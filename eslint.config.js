b43e0582eee32fd48b2933a45b772bedc9dd6eca
Locietta <locietta@gmail.com>
2024-12-30 14:57:24 +0800

    chore: revive eslint & cleanup dead deps

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import eslint from '@eslint/js'
+import eslintConfigPrettier from '@vue/eslint-config-prettier'
+import eslintPluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+import typescriptEslint from 'typescript-eslint'
+
+export default typescriptEslint.config(
+  { ignores: ['*.d.ts', '**/coverage', '**/dist', '**/cache', 'node_modules/**/*'] },
+  {
+    extends: [eslint.configs.recommended, ...typescriptEslint.configs.recommended, ...eslintPluginVue.configs['flat/recommended']],
+    files: ['**/*.{ts,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        parser: typescriptEslint.parser
+      }
+    },
+    rules: {
+      'array-bracket-newline': ['error', 'consistent'],
+      'vue/multi-word-component-names': 0
+    }
+  },
+  eslintConfigPrettier
+)
