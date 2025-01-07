288c912665a17827038bb87ae923a92b12bc8f98
Locietta <locietta@gmail.com>
2025-01-07 14:26:07 +0800

    misc: ignore lint in type decl files

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import globals from 'globals'
 import typescriptEslint from 'typescript-eslint'
 
 export default typescriptEslint.config(
-  { ignores: ['*.d.ts', '**/coverage', '**/dist', '**/cache', 'node_modules/**/*'] },
+  { ignores: ['**/*.d.ts', '**/coverage', '**/dist', '**/cache', 'node_modules/**/*'] },
   {
     extends: [
       eslint.configs.recommended,
